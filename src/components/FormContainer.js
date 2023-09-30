0cec5641a97ae2b38190eb4b83109858dbcec364
RaghRoog <szumon612@interia.pl>
2023-09-30 15:10:31 +0200

    taking user input

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -1,36 +1,65 @@
+import { useState } from "react"
 
-export default function FormContainer({section, deleteSection}) {
+export default function FormContainer({section, deleteSection, input, setInput}) {
+
+    function getInput(inputType, id, key, value, setFunc){
+        let index = inputType.findIndex(obj => obj.id == id)
+        if(index != -1){
+            inputType[index][key] = value
+        }else{
+            setFunc([...inputType, {id: id, [key]: value}])
+        }
+    }
 
     return(
-        <div className="form-container">
-            <form key={section.id}>
+        <div id={section.id} className="form-container">
+            <form>
                 <div className="input-container">
                     <label htmlFor={`${section.name}${section.id}`}>{section.labels[0]}</label>
-                    <input type="text" name={`${section.name}${section.id}`} id={`${section.name}${section.id}`} />
+                    <input onChange={(e)=>getInput(input, section.id, section.labels[0],
+                        e.target.value, setInput)} type="text"
+                        id={`${section.name}${section.id}`} 
+                        //value={inputObj.section.label[0]}
+                    />
                 </div>
                 {section.name != 'course' ? (<div className="input-container">
                     <label htmlFor={`${section.name}City${section.id}`}>City</label>
-                    <input type="text" name={`${section.name}City${section.id}`} id={`${section.name}City${section.id}`} />
+                    <input onChange={(e)=>getInput(input, section.id, 'City', 
+                        e.target.value, setInput)} type="text" 
+                        id={`${section.name}City${section.id}`} 
+                    />
                 </div>) : null}
-                
                 <div className="input-container">
-                    <label htmlFor={`employer${section.id}`}>{section.labels[1]}</label>
-                    <input type="text" name={`employer${section.id}`} id={`employer${section.id}`} />
+                    <label htmlFor={`${section.name}${section.id}`}>{section.labels[1]}</label>
+                    <input onChange={(e)=>getInput(input, section.id, section.labels[1], 
+                        e.target.value, setInput)} type="text" 
+                        id={`${section.name}${section.id}`} 
+                    />
                 </div>
                 <div className="input-container">
                     <label htmlFor={`${section.name}Start${section.id}`}>Start date</label>
-                    <input type="date" name={`${section.name}Start${section.id}`} id={`${section.name}Start${section.id}`} />
+                    <input onChange={(e)=>getInput(input, section.id, 'StartDate', 
+                        e.target.value, setInput)} type="date" 
+                        id={`${section.name}Start${section.id}`} 
+                    />
                 </div>
                 <div className="input-container">
                     <label htmlFor={`${section.name}End${section.id}`}>End date</label>
-                    <input type="date" name={`${section.name}End${section.id}`} id={`${section.name}End${section.id}`} />
+                    <input onChange={(e)=>getInput(input, section.id, 'EndDate', 
+                        e.target.value, setInput)} type="date" 
+                        id={`${section.name}End${section.id}`} 
+                    />
                 </div>
                 <div className="input-container">
                     <label htmlFor={`${section.name}Desc${section.id}`}>Description</label>
-                    <textarea id={`${section.name}Desc${section.id}`} name={`${section.name}Desc${section.id}`}></textarea>
+                    <textarea onChange={(e)=>getInput(input, section.id, 'desc', 
+                        e.target.value, setInput)} id={`${section.name}Desc${section.id}`}>
+                    </textarea>
                 </div>
             </form>
-            <button onClick={()=>deleteSection(section.id, section.name)} className="delete-btn">Delete</button>
+            <button onClick={()=>deleteSection(section.id, section.name)} 
+                className="delete-btn">Delete
+            </button>
         </div>
     )
 }
--- src/components/InputSection.js
+++ src/components/InputSection.js
@@ -1,8 +1,9 @@
 import { useState } from "react"
 import FormContainer from "./FormContainer"
 
-export default function InputSection() {
+export default function InputSection({ getIsGenerated, getInputsData }) {
 
+    //adding sections
     let [edu, setEdu] = useState([])
     let [job, setJob] = useState([])
     let [course, setCourse] = useState([])
@@ -11,20 +12,36 @@ export default function InputSection() {
     let [langs, setLangs] = useState([])
     let [links, setLinks] = useState([])
 
-    function addSection(name, label1, label2, sectionType) {
-        let newId = sectionType.length + 1
-        let section = { id: newId, labels: [label1, label2], name: name  }
+    function findNewId(array){
+        let id = 0;
+        if (array.length > 0) {
+          id = Math.max(...array.map(item => item.id)) + 1;
+        }
+        return id;
+    }
+
+    function addSection(name, label1, label2) {
+        let newId
+        let section = { labels: [label1, label2], name: name  }
         switch(name){
             case 'job':
+                newId  = findNewId(job)
+                section.id = newId
                 setJob([...job, section])
                 break;
             case 'education':
+                newId = findNewId(edu)
+                section.id = newId
                 setEdu([...edu, section])
                 break;
             case 'course':
+                newId = findNewId(course)
+                section.id = newId
                 setCourse([...course, section])
                 break;
             case 'certificates':
+                newId = findNewId(certificates)
+                section.id = newId
                 setCertificates([...certificates, section])
                 break;
         }
@@ -35,15 +52,19 @@ export default function InputSection() {
         switch(name){
             case 'job':
                 setJob(job.filter(item => item.id != id))
+                setJobInput(jobInput.filter(item => item.id != id))
                 break;
             case 'education':
                 setEdu(edu.filter(item => item.id != id))
+                setEduInput(eduInput.filter(item => item.id != id))
                 break;
             case 'course':
                 setCourse(course.filter(item => item.id != id))
+                setCoursesInput(coursesInput.filter(item => item.id != id))
                 break;
             case 'certificates':
                 setCertificates(certificates.filter(item => item.id != id))
+                setCertificatesInput(certificatesInput.filter(item => item.id != id))
                 break;
         }
     }
@@ -53,12 +74,12 @@ export default function InputSection() {
         let newItem
         switch(type){
             case 'skill':
-                newId = skills.length + 1
+                newId = findNewId(skills)
                 newItem = {id: newId}
                 setSkills([...skills, newItem])
                 break;
             case 'lang':
-                newId = langs + 1
+                newId = findNewId(langs)
                 newItem = {id: newId}
                 setLangs([...langs, newItem])
                 break;
@@ -70,22 +91,122 @@ export default function InputSection() {
         switch(type){
             case 'skill':
                 setSkills(skills.filter(skill => skill.id != id))
+                setSkillsInput(skillsInput.filter(skill => skill.id != id))
                 break;
             case 'lang':
                 setLangs(langs.filter(lang => lang.id != id))
+                setLangsInput(langsInput.filter(lang => lang.id != id))
                 break;
         }
         
     }
 
     function addLink(){
-        let newId = links.length + 1
+        let newId = findNewId(links)
         let newLink = {id: newId}
         setLinks([...links, newLink])
     }
 
     function deleteLink(id){
         setLinks(links.filter(link => link.id != id))
+        setLinksInput(linksInput.filter(link => link.id != id))
+    }
+
+    //taking user input
+    //details
+    let [detailsInput, setDetailsInput] = useState({})
+    function getDetails(key, value){
+        setDetailsInput({...detailsInput, [key]: value})
+    }
+    //about you
+    let [aboutInput, setAboutInput] = useState('')
+    //certificates 
+    let [certificatesInput, setCertificatesInput] = useState([])
+    function getCertificates(id, value){
+        let newCertificate = {id: id, cert: value}
+        let index = certificatesInput.findIndex(obj => obj.id === id)
+        if(index != -1){
+            certificatesInput[index] = newCertificate
+        }else{
+            setCertificatesInput([...certificatesInput, newCertificate])
+        }
+    }
+    //skills and languages
+    let [skillsInput, setSkillsInput] = useState([])
+    let [langsInput, setLangsInput] = useState([])
+    function getInputName(id, value, inputType){
+        let index
+        switch(inputType){
+            case 'skills':
+                index = skillsInput.findIndex(obj => obj.id == id)
+                if(index != -1){
+                    skillsInput[index].skillName = value
+                }else{
+                    setSkillsInput([...skillsInput, {id: id, skillName: value}])
+                }
+                break
+            case 'langs':
+                index = langsInput.findIndex(obj => obj.id == id)
+                if(index != -1){
+                    langsInput[index].language = value
+                }else{
+                    setLangsInput([...langsInput, {id: id, language: value}])
+                }
+                break
+        }
+        
+    }
+    function getInputLvl(id, value, inputType){
+        let index
+        switch(inputType){
+            case 'skills':
+                index = skillsInput.findIndex(obj => obj.id == id)
+                if(index != -1){
+                    skillsInput[index].skillLvl = value
+                }else{
+                    setSkillsInput([...skillsInput, {id: id, skillLvl: value}])
+                }
+                break
+            case 'langs':
+                index = langsInput.findIndex(obj => obj.id == id)
+                if(index != -1){
+                    langsInput[index].langLvl = value
+                }else{
+                    setLangsInput([...langsInput, {id: id, langLvl: value}])
+                }
+                break
+        }
+
+    }
+    //links
+    let [linksInput, setLinksInput] = useState([])
+    function getLink(id, value, type){
+        let index = linksInput.findIndex(obj => obj.id == id)
+        if(index != -1){
+            linksInput[index][type] = value
+        }else{
+            setLinksInput([...linksInput, {id: id, [type]: value}])
+        }
+    }
+    //taking input from FormContainer component
+    let [eduInput, setEduInput] = useState([])
+    let [jobInput, setJobInput] = useState([])
+    let [coursesInput, setCoursesInput] = useState([])
+
+    function generateResume(){
+        getIsGenerated(true)
+        let inputData = {
+            details: detailsInput,
+            about: aboutInput,
+            certificates: certificatesInput,
+            skills: skillsInput,
+            languages: langsInput,
+            links: linksInput,
+            edu: eduInput,
+            job: jobInput,
+            courses: coursesInput
+        }
+        getInputsData(inputData)
     }
 
     return(
@@ -95,76 +216,95 @@ export default function InputSection() {
                 <form>
                     <div className="input-container">
                         <label htmlFor="photo">Photo</label>
-                        <input type="file" accept="image/*" name="photo" id="photo" />
+                        <input type="file" accept="image/*" id="photo" />
                     </div>
                     <div className="input-container">
                         <label htmlFor="fname">First name</label>
-                        <input type="text" name="fname" id="fname" />
+                        <input onChange={(e)=>getDetails('fname', e.target.value)} type="text" id="fname" />
                     </div>
                     <div className="input-container">
                         <label htmlFor="lname">Last name</label>
-                        <input type="text" name="lname" id="lname" />
+                        <input onChange={(e)=>getDetails('lname', e.target.value)} type="text" id="lname" />
                     </div>
                     <div className="input-container">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="email" />
+                        <input onChange={(e)=>getDetails('email', e.target.value)} type="email" id="email" />
                     </div>
                     <div className="input-container">
                         <label htmlFor="phone">Phone</label>
-                        <input type="tel" name="phone" id="phone" />
+                        <input onChange={(e)=>getDetails('tel', e.target.value)} type="tel" id="phone" />
                     </div>
                     <div className="input-container">
                         <label htmlFor="address">Address</label>
-                        <input type="text" name="address" id="address" />
+                        <input onChange={(e)=>getDetails('address', e.target.value)} type="text" id="address" />
                     </div>
                     <div className="input-container">
                         <label htmlFor="city">City</label>
-                        <input type="text" name="city" id="city" />
+                        <input onChange={(e)=>getDetails('city', e.target.value)} type="text" id="city" />
                     </div>
                     <div className="input-container">
                         <label htmlFor="country">Country</label>
-                        <input type="text" name="country" id="country" />
+                        <input onChange={(e)=>getDetails('country', e.target.value)} type="text" id="country" />
                     </div>
                 </form>
             </div>
             <h3>About you</h3>
             <div className="form-container">
                 <small>Max 500 characters</small>
-                <textarea name="about" id="about" 
+                <textarea onChange={(e)=>setAboutInput(e.target.value)} id="about" 
                  cols="30" rows="10" maxLength={"500"} style={{width: '100%', height: '150px'}}></textarea>
             </div>
             <h3>Education</h3>
             {edu.map(section => (
-                <FormContainer section={section} deleteSection={deleteSection}/>
+                <FormContainer key={section.id} section={section} deleteSection={deleteSection} 
+                    input={eduInput} setInput={setEduInput}
+                />
             ))}
-            <button onClick={()=>addSection('education', 'School', 'Degree', edu)} className="btn-secondary">Add education</button>
+            <button onClick={()=>addSection('education', 'School', 'Degree')} 
+                className="btn-secondary">Add education
+            </button>
             <h3>Employment history</h3>
             {job.map(section => (
-                <FormContainer section={section} deleteSection={deleteSection}/>
+                <FormContainer key={section.id} section={section} deleteSection={deleteSection} 
+                    input={jobInput} setInput={setJobInput}
+                />
             ))}
-            <button onClick={()=>addSection('job', 'Job title', 'Employer', job)} className="btn-secondary">Add employement</button>
+            <button onClick={()=>addSection('job', 'Job title', 'Employer')} 
+                className="btn-secondary">Add employement
+            </button>
             <h3>Courses</h3>
             {course.map(section => (
-                <FormContainer section={section} deleteSection={deleteSection}/>
+                <FormContainer key={section.id} section={section} deleteSection={deleteSection} 
+                    input={coursesInput} setInput={setCoursesInput}
+                />
             ))}
-            <button onClick={()=>addSection('course', 'Course', 'Institution', course)} className="btn-secondary">Add course</button>
+            <button onClick={()=>addSection('course', 'Course', 'Institution')} 
+                className="btn-secondary">Add course
+            </button>
             <h3>Certificates</h3>
             {certificates.map(section => (
                 <div key={section.id} className="form-container">
                     <small>Max 100 characters</small>
-                    <textarea name="about" id="about" 
+                    <textarea onChange={(e)=>getCertificates(section.id, e.target.value)} id="about" 
                      cols="30" rows="10" maxLength={"100"} style={{width: '100%', height: '50px'}}></textarea>
                     <button onClick={()=> deleteSection(section.id, 'certificates')} className="delete-btn">Delete</button>
                 </div>
             ))}
-            <button onClick={()=>addSection('certificates', '', '', certificates)} className="btn-secondary">Add certificate</button>
+            <button onClick={()=>addSection('certificates', '', '')} 
+                className="btn-secondary">Add certificate
+            </button>
             <h3>Skills</h3>
-            <div className="container" style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly'}}>
+            <div className="container" style={{display: 'flex', flexWrap: 'wrap', gap: '1rem'}}>
                 {skills.map(skill => (
                     <div key={skill.id} className="form-container" style={{width: 'fit-content'}}>
-                        <form style={{flexWrap: 'nowrap'}}>
-                            <input type="text" />
-                            <select name="skillLvl" id="sklil">
+                        <form>
+                            <input onChange={(e)=>getInputName(skill.id, e.target.value, 'skills')} 
+                                type="text" style={{minWidth: '150px'}}
+                            />
+                            <select onChange={(e)=>getInputLvl(skill.id, e.target.value, 'skills')} 
+                                name="skillLvl" id="sklil"
+                            >
+                                <option>Choose your level</option>
                                 <option value="basics">Basics</option>
                                 <option value="intermediate">Intermediate</option>
                                 <option value="advanced">Advanced</option>
@@ -177,12 +317,17 @@ export default function InputSection() {
             </div>
             <button onClick={()=>addSkillOrLang('skill')} className="btn-secondary">Add skill</button>
             <h3>Languages</h3>
-            <div className="container" style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly'}}>
+            <div className="container" style={{display: 'flex', flexWrap: 'wrap', gap: '1rem'}}>
                 {langs.map(lang => (
                     <div key={lang.id} className="form-container" style={{width: 'fit-content'}}>
-                        <form style={{flexWrap: 'nowrap'}}>
-                            <input type="text" />
-                            <select name="langLvl" id="lang">
+                        <form>
+                            <input onChange={(e)=>getInputName(lang.id, e.target.value, 'langs')} 
+                                type="text" 
+                            />
+                            <select onChange={(e)=>getInputLvl(lang.id, e.target.value, 'langs')} 
+                                id="lang" style={{minWidth: '150px'}}
+                            >
+                                <option>Choose your level</option>
                                 <option value="basics">Basics</option>
                                 <option value="intermediate">Intermediate</option>
                                 <option value="advanced">Advanced</option>
@@ -197,20 +342,29 @@ export default function InputSection() {
             <h3>Links</h3>
                 {links.map(link => (
                     <div key={link.id} className="form-container">
-                        <form style={{flexWrap: 'nowrap'}}>
+                        <form style={{flexWrap: 'nowrap', flexDirection: 'column'}}>
                             <div className="input-container">
                                 <label htmlFor={'linkName' + link.id}>Page name</label>
-                                <input type="text" name={'linkName' + link.id} id={'linkName' + link.id} />
+                                <input onChange={(e)=>getLink(link.id, e.target.value, 'name')} 
+                                    type="text" name={'linkName' + link.id} 
+                                    id={'linkName' + link.id} 
+                                />
                             </div>
                             <div className="input-container">
                                 <label htmlFor={'link' + link.id}>Link</label>
-                                <input type="text" name={'link' + link.id} id={'link' + link.id} />
+                                <input onChange={(e)=>getLink(link.id, e.target.value, 'link')} 
+                                    type="text" name={'link' + link.id} 
+                                    id={'link' + link.id} 
+                                />
                             </div>
                         </form>
                         <button onClick={()=> deleteLink(link.id)} className="delete-btn">Delete</button>
                     </div>
                 ))}
             <button onClick={addLink} className="btn-secondary">Add link</button>
+            <div className="container" style={{display:'flex', justifyContent:'center'}}>
+                <button onClick={generateResume} className="btn-primary">Generate resume</button>
+            </div>
         </div>
     )
 }
